0ee96395aa9cb70674f4a819ad4ab68c7f84e605
Eric D. Schabell <eric@schabell.org>
2020-09-23 13:53:31 +0200

    Updated readme for rest swagger testing.

--- projects/application-ui/config/config.js
+++ projects/application-ui/config/config.js
@@ -3,7 +3,7 @@
 var ENV = {
     'kieserver_host' : 'insecure-quick-loan-bank-kieserver-appdev-in-cloud.apps-crc.testing',
     'kieserver_port' : '80',
-    'kieserver_contextroot' : '/services/rest/server/containers/instances',
+    'kieserver_contextroot' : '/',
     'kieserver_containerId' : 'loan-application_1.0',
     'kieserver_user' : 'erics',
     'kieserver_password' : 'redhatdm!'
