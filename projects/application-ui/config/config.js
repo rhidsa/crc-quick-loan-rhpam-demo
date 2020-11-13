350de6b2ea33f05a5dbc6579e11a559e32def3de
Eric D. Schabell <eric@schabell.org>
2020-11-13 14:57:40 +0100

    Updated to work with new loan pre approval renaming sequence.

--- projects/application-ui/config/config.js
+++ projects/application-ui/config/config.js
@@ -4,7 +4,7 @@ var ENV = {
     'kieserver_host' : 'insecure-quick-loan-bank-kieserver-appdev-in-cloud.apps-crc.testing',
     'kieserver_port' : '80',
     'kieserver_contextroot' : '',
-    'kieserver_containerId' : 'loan-application_1.0',
+    'kieserver_containerId' : 'loan-pre-approval_1.0-SNAPSHOT',
     'kieserver_user' : 'erics',
     'kieserver_password' : 'redhatdm1!'
 };
